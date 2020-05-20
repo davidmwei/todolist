169f658472639fe0cfc54bac18ce32d2d0b1655d
David Wei <davidwei@berkeley.edu>
2020-05-20 15:49:06 -0700

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    render() {
+        return (
+            <div className = "todoListMain">
+                <div className = "header">
+                </div>
+            </div>
+        )
+    }
+}
