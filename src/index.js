f2f8de5b8ce5a1d10a480f166bcb059766672f72
David Wei <davidwei@berkeley.edu>
2020-05-20 17:07:03 -0700

    finish rudimentary todo list

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+import './index.css';
+
+var root = document.getElementById('root');
+
+ReactDOM.render(
+    <TodoList />,
+    root
+);
